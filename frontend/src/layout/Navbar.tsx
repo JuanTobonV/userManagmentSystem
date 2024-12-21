9a1b9d338efc311a3ef5254623f5f896e8e80a3d
Juan Villa <jujotovil@gmail.com>
2024-12-21 00:22:27 -0500

    installing tailwind

---
+++ frontend/src/layout/Navbar.tsx
@@ -0,0 +1,11 @@
+
+export default function Navbar() {
+  return (
+    <>
+       <nav className="bg-blue-300 w-full flex justify-center gap-7 p-4">
+            <a className="text-xl font-medium border-b border-gray-500 p-2 pb-1 text-gray-700" href="">Add user</a>
+            <a className="text-xl font-medium p-2 pb-1 text-gray-700" href="">About</a>
+       </nav>
+    </>
+  )
+}
