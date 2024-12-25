38b5ba7b38668756053df137661509efa1b1b281
Juan Villa <jujotovil@gmail.com>
2024-12-24 23:29:24 -0500

    adding put and delete endpoints

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -42,7 +42,7 @@ export default function Home() {
                                 <td>{user.email}</td>
                                 <td className='flex gap-2 justify-center mt-3'>
                                     <button className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800  focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">View</button>
-                                    <button type="button" className="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500  focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Edit</button>
+                                    <button type="button" className="text-black hover:text-dark border border-black hover:bg-white  focus:outline-none focus:ring-black-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-black-300 dark:text-black-300 dark:hover:text-white dark:hover:bg-gray-400 dark:focus:ring-yellow-900">Edit</button>
                                     <button type="button" className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800  focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">Delete</button>
                                 </td>
                             </tr>
